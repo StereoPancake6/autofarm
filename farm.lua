08c972b8299f0087afaadf186c4fae719bc0e857
StereoPancake6 <94248949+StereoPancake6@users.noreply.github.com>
2022-12-26 00:14:56 -0800

    Update farm.lua

--- farm.lua
+++ farm.lua
@@ -1,37 +1,88 @@
-local humanoid = game.Players.LocalPlayer.Character.Humanoid
-local tool = game.Players.LocalPlayer.Backpack.Combat
-
-local function getMoneyAroundMe() 
-    wait(0.7)
-    for i, money in ipairs(game.Workspace.Ignored.Drop:GetChildren()) do
-        if money.Name == "MoneyDrop" and (money.Position - game.Players.LocalPlayer.Character.HumanoidRootPart.Position).magnitude <= 20 then
-            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = money.CFrame
-            fireclickdetector(money.ClickDetector)
-            wait(0.6)
-        end  
-    end
-end
-
-
-local function startAutoFarm() 
-    humanoid:EquipTool(tool)
-
-    for i, v in ipairs(game.Workspace.Cashiers:GetChildren()) do
-        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v.Open.CFrame * CFrame.new(0, 0, 2)
-
-        for i = 0, 15 do
-            wait(0.7)
-            tool:Activate()
+--[[
+Fortniyr
+]]
+repeat
+    wait()
+until game:IsLoaded()
+local gm = getrawmetatable(game)
+setreadonly(gm, false)
+local namecall = gm.__namecall
+gm.__namecall =
+    newcclosure(
+    function(self, ...)
+        local args = {...}
+        if not checkcaller() and getnamecallmethod() == "FireServer" and tostring(self) == "MainEvent" then
+            if tostring(getcallingscript()) ~= "Framework" then
+                return
+            end
         end
-
-        getMoneyAroundMe()
-
+        if not checkcaller() and getnamecallmethod() == "Kick" then
+            return
+        end
+        return namecall(self, unpack(args))
     end
+)
 
+local LocalPlayer = game:GetService("Players").LocalPlayer
 
-
-    wait(0.6)
- 
+function gettarget()
+    local Character = LocalPlayer.Character or LocalPlayer.CharacterAdded:wait()
+    local HumanoidRootPart = Character:WaitForChild("HumanoidRootPart")
+    local maxdistance = math.huge
+    local target
+    for i, v in pairs(game:GetService("Workspace").Cashiers:GetChildren()) do
+        if v:FindFirstChild("Head") and v:FindFirstChild("Humanoid") and v.Humanoid.Health > 0 then
+            local distance = (HumanoidRootPart.Position - v.Head.Position).magnitude
+            if distance < maxdistance then
+                target = v
+                maxdistance = distance
+            end
+        end
+    end
+    return target
 end
 
-startAutoFarm()
+for i, v in pairs(workspace:GetDescendants()) do
+    if v:IsA("Seat") then
+        v:Destroy()
+    end
+end
+
+print("Amnesia's Da Hood Farm")
+
+shared.MoneyFarm = true -- Just execute shared.MoneyFarm = false to stop farming
+
+while shared.MoneyFarm do
+    wait()
+    local Target = gettarget()
+    repeat
+        wait()
+        pcall(
+            function()
+                local Character = LocalPlayer.Character or LocalPlayer.CharacterAdded:wait()
+                local HumanoidRootPart = Character:WaitForChild("HumanoidRootPart")
+                local Combat = LocalPlayer.Backpack:FindFirstChild("Combat") or Character:FindFirstChild("Combat")
+                if not Combat then
+                    Character:FindFirstChild("Humanoid").Health = 0
+                    return
+                end
+                HumanoidRootPart.CFrame = Target.Head.CFrame * CFrame.new(0, -2.5, 3)
+                Combat.Parent = Character
+                Combat:Activate()
+            end
+        )
+    until not Target or Target.Humanoid.Health < 0
+    for i, v in pairs(game:GetService("Workspace").Ignored.Drop:GetDescendants()) do
+        if v:IsA("ClickDetector") and v.Parent and v.Parent.Name:find("Money") then
+            local Character = LocalPlayer.Character or LocalPlayer.CharacterAdded:wait()
+            local HumanoidRootPart = Character:WaitForChild("HumanoidRootPart")
+            if (v.Parent.Position - HumanoidRootPart.Position).magnitude <= 18 then
+                repeat
+                    wait()
+                    fireclickdetector(v)
+                until not v or not v.Parent.Parent
+            end
+        end
+    end
+    wait(1)
+end
